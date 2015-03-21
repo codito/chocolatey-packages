662fda06ad66e06aa8ff65054f0030116ecec4f0
Arun Mahapatra <pratikarun@gmail.com>
2015-03-21 12:35:06 +0000

    Add insomnia package.

---
+++ insomnia/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$packageName = 'insomnia'
+$url = 'http://dlaa.me/Samples/Insomnia/Insomnia.zip'
+
+try
+{
+    $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+    Install-ChocolateyZipPackage -PackageName "$packageName" -Url "$url" -UnzipLocation "$installDir" -Url64bit ""
+}
+catch {
+    throw $_.Exception
+}
---
+++ insomnia/tools/chocolateyuninstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'insomnia'
+
+try
+{
+    $osBitness = Get-ProcessorBits
+    $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+    Write-Host "Stopping insomnia if running..."
+    Get-Process | Where-Object { $_.Name -like $packageName } | Stop-Process
+
+    Write-Host "Removing $installDir"
+    Remove-Item -Recurse -Force $installDir
+}
+catch {
+  Write-ChocolateyFailure $packageName "$($_.Exception.Message)"
+  throw
+}
